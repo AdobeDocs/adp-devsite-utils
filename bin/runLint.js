944643a0035de0e4389ca40aec7cbdd334a132a8
Louisa Chu <louisa@ensemble.com>
2025-08-15 16:56:50 -0700

    go back to plugin

--- bin/runLint.js
+++ bin/runLint.js
@@ -11,8 +11,7 @@ const adpDevsiteUtilsDir = path.dirname(__dirname);
 
 // Array of plugins to use - easily add/remove plugins here
 const PLUGINS = [
-    'remark-lint-no-multiple-toplevel-headings',
-    'remark-validate-links'
+    'remark-lint-no-multiple-toplevel-headings'
     // 'remark-lint-no-dead-links',
     // 'remark-lint-no-empty-url'
 ];
