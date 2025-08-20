dc21c7d0a81aaba7c3b29c2dbfeac6b699a2d8f0
Louisa Chu <louisa@ensemble.com>
2025-08-20 10:48:49 -0700

    update

--- linter/remark-lint-no-angle-brackets.js
+++ linter/remark-lint-no-angle-brackets.js
@@ -2,34 +2,35 @@ import { visit } from 'unist-util-visit'
 
 const remarkLintNoAngleBrackets = () => {
   return (tree, file) => {
-    // Visit all nodes to find angle bracket links
-    visit(tree, (node) => {
-      if (node.type === 'link') {
-        // Check if this link has no text (just URL) which suggests angle bracket syntax
-        if (node.children && node.children.length === 1 && node.children[0].type === 'text') {
-          const linkText = node.children[0].value
-          const url = node.url
-          
-          // If the link text is the same as the URL, it might be using angle bracket syntax
-          if (linkText === url && (url.startsWith('http://') || url.startsWith('https://') || url.startsWith('www.'))) {
-            const position = {
-              start: {
-                line: node.position.start.line,
-                column: node.position.start.column
-              },
-              end: {
-                line: node.position.end.line,
-                column: node.position.end.column
-              }
-            }
-            
-            file.message(
-              `Use [link text](${url}) instead of <${url}> for better accessibility`,
-              position,
-              'remark-lint:no-angle-brackets'
-            )
+    // Visit all nodes to find actual angle bracket syntax
+    visit(tree, 'text', (node) => {
+      const text = node.value
+
+      // Look for patterns like <http://...> or <https://...>
+      const angleBracketUrlRegex = /<((?:https?:\/\/|www\.)[^>]+)>/g
+      let match
+
+      while ((match = angleBracketUrlRegex.exec(text)) !== null) {
+        const url = match[1]
+        const startColumn = node.position.start.column + match.index
+        const endColumn = startColumn + match[0].length
+
+        const position = {
+          start: {
+            line: node.position.start.line,
+            column: startColumn
+          },
+          end: {
+            line: node.position.end.line,
+            column: endColumn
           }
         }
+
+        file.message(
+          `Use [link text](${url}) instead of <${url}> for better accessibility`,
+          position,
+          'remark-lint:no-angle-brackets'
+        )
       }
     })
   }
