497a8db387d34832a3c581aeafbb7117ed6216c1
Louisa Chu <louisa@ensemble.com>
2025-08-20 13:55:27 -0700

    update

--- linter/remark-lint-no-angle-brackets.js
+++ linter/remark-lint-no-angle-brackets.js
@@ -1,50 +1,30 @@
 import { visit } from 'unist-util-visit'
 
-const remarkLintNoAngleBrackets = (severity = 'warn') => {
+const remarkLintNoAngleBrackets = () => {
   return (tree, file) => {
-    // Visit all nodes to find actual angle bracket syntax
-    visit(tree, 'text', (node) => {
-      const text = node.value
+    // Visit all nodes to find angle bracket links
+    visit(tree, (node) => {
+      if (node.type === 'link') {
+        // Check if this link has no text (just URL) which suggests angle bracket syntax
+        if (node.children && node.children.length === 1 && node.children[0].type === 'text') {
+          const linkText = node.children[0].value
+          const url = node.url
 
-      // Debug: log what we're seeing
-      console.log('Checking text node:', text)
-
-      // Look for content inside angle brackets that matches link patterns
-      const angleBracketRegex = /<([^>]+)>/g
-      let match
-
-      while ((match = angleBracketRegex.exec(text)) !== null) {
-        const content = match[1]
-        console.log('Found angle bracket content:', content)
-
-        // Check if the content matches a link pattern
-        const linkPattern = /^(https?:\/\/|www\.|mailto:)/
-        if (linkPattern.test(content)) {
-          console.log('Content matches link pattern:', content)
-
-          const startColumn = node.position.start.column + match.index
-          const endColumn = startColumn + match[0].length
-
-          const position = {
-            start: {
-              line: node.position.start.line,
-              column: startColumn
-            },
-            end: {
-              line: node.position.end.line,
-              column: endColumn
+          // If the link text is the same as the URL, it might be using angle bracket syntax
+          if (linkText === url && (url.startsWith('http://') || url.startsWith('https://') || url.startsWith('www.'))) {
+            const position = {
+              start: {
+                line: node.position.start.line,
+                column: node.position.start.column
+              },
+              end: {
+                line: node.position.end.line,
+                column: node.position.end.column
+              }
             }
-          }
 
-          // Use severity option to determine if it's a warning or error
-          if (severity === 'error') {
-            file.fail(
-              `Use [link text](${content}) instead of <${content}> for better accessibility`,
-              position
-            )
-          } else {
             file.message(
-              `Use [link text](${content}) instead of <${content}> for better accessibility`,
+              `Use [link text](${url}) instead of <${url}> for better accessibility`,
               position,
               'remark-lint:no-angle-brackets'
             )
