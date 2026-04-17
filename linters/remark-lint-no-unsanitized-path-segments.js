2e5bec3992b80153aeabc44b7d4922df150b1830
yuxuanj <yuxuanj@adobe.com>
2026-04-17 10:29:49 -0700

    updated linter name, replaced split

--- linters/remark-lint-no-unsanitized-path-segments.js
+++ linters/remark-lint-no-unsanitized-path-segments.js
@@ -1,4 +1,6 @@
-const RULE_ID = 'remark-lint:no-kebab-in-filename'
+import path from 'path'
+
+const RULE_ID = 'remark-lint:no-unsanitized-path-segments'
 
 const POSITION = {
   start: { line: 1, column: 1 },
@@ -30,11 +32,11 @@ function validateSegment(segment, label, file, severity) {
   }
 }
 
-const remarkLintNoKebabInFilename = (severity = 'warning') => {
+const remarkLintNoUnsanitizedPathSegments = (severity = 'warning') => {
   return (tree, file) => {
     const actualSeverity = Array.isArray(severity) ? severity[0] : severity
 
-    const filename = file.basename || (file.path ? file.path.split('/').pop() : null)
+    const filename = file.basename || (file.path ? path.basename(file.path) : null)
 
     if (!filename) {
       return
@@ -44,10 +46,11 @@ const remarkLintNoKebabInFilename = (severity = 'warning') => {
     validateSegment(filenameWithoutExt, 'Filename', file, actualSeverity)
 
     if (file.path) {
-      const srcPagesIndex = file.path.indexOf('src/pages/')
+      const srcPagesMarker = path.join('src', 'pages')
+      const srcPagesIndex = file.path.indexOf(srcPagesMarker)
       if (srcPagesIndex !== -1) {
-        const relativePath = file.path.slice(srcPagesIndex + 'src/pages/'.length)
-        const segments = relativePath.split('/')
+        const relativePath = file.path.slice(srcPagesIndex + srcPagesMarker.length + path.sep.length)
+        const segments = relativePath.split(path.sep)
         segments.pop() // remove filename (already checked above)
         for (const dir of segments) {
           if (dir) {
@@ -59,4 +62,4 @@ const remarkLintNoKebabInFilename = (severity = 'warning') => {
   }
 }
 
-export default remarkLintNoKebabInFilename
+export default remarkLintNoUnsanitizedPathSegments
